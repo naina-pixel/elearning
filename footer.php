4ab193d516b94e7cef3b3a95724434b9596f4712
unknown <nainabarnal@gmail.com>
2025-05-02 22:40:35 +0530

    footer updated

--- footer.php
+++ footer.php
@@ -79,7 +79,7 @@
                         <div class="col-xl-12 ">
                             <div class="footer-copy-right text-center">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="" target="_blank"></a>
+                                  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This Project is made by Mutki <i class="fa fa-heart" aria-hidden="true"></i> <a href="" target="_blank"></a>
                                   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                               </div>
                           </div>
